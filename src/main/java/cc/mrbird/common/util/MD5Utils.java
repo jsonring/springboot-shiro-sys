9c49a7360091bdc9e2e2dfe9a1597153f1776d24
USER-20180707YH\Administrator <m17710633231@163.com>
2019-06-04 00:03:05 +0800

    11

--- src/main/java/cc/mrbird/common/util/MD5Utils.java
+++ src/main/java/cc/mrbird/common/util/MD5Utils.java
@@ -3,25 +3,115 @@ package cc.mrbird.common.util;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 
+import java.security.MessageDigest;
+
 public class MD5Utils {
 
-	protected MD5Utils(){
+    protected MD5Utils() {
 
-	}
+    }
 
-	private static final String SALT = "mrbird";
+    private static final String SALT = "mrbird";
 
-	private static final String ALGORITH_NAME = "md5";
+    private static final String ALGORITH_NAME = "md5";
 
-	private static final int HASH_ITERATIONS = 2;
+    private static final int HASH_ITERATIONS = 2;
 
-	public static String encrypt(String pswd) {
-		return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(SALT), HASH_ITERATIONS).toHex();
-	}
+    public static String encrypt(String pswd) {
+        return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(SALT), HASH_ITERATIONS).toHex();
+    }
 
-	public static String encrypt(String username, String pswd) {
-		return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(username.toLowerCase() + SALT),
-				HASH_ITERATIONS).toHex();
-	}
+    /**
+     * 32位加密
+     *
+     * @param username
+     * @param pswd
+     * @return
+     */
+    public static String encrypt(String username, String pswd) {
+        return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(username.toLowerCase() + SALT),
+                HASH_ITERATIONS).toHex();
+    }
 
+    /**
+     * md5 32位加密
+     *
+     * @param pswd
+     * @return
+     */
+    public static String MD5(String pswd) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f'};
+        try {
+            byte[] strTemp = pswd.getBytes();
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * MD5加码。32位
+     *
+     * @param inStr
+     * @return
+     */
+    public static String getMd5(String inStr) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+
+        byte[] md5Bytes = md5.digest(byteArray);
+
+        StringBuffer hexValue = new StringBuffer();
+
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+
+        return hexValue.toString();
+    }
+
+    // 可逆的自定义加密算法
+    public static String KL(String pswd) {
+        // String s = new String(inStr);
+        char[] a = pswd.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (char) (a[i] ^ 't');
+        }
+        String s = new String(a);
+        return s;
+    }
+
+    //测试加密
+    public static void main(String[] args) {
+//		String addSalt = encrypt("user1", "1234567");
+//		System.out.println(addSalt+"\n"+addSalt.length());
+        String s = KL("123456");
+        System.out.println(s + "\n" + s.length());
+    }
 }
